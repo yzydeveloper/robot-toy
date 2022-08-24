50b9045b1059cd4d8e6c193c279d36533dba2abb
yzydeveloper <wqrdw@qq.com>
2022-08-24 10:53:58 +0800

    feat: define wecom api

--- packages/shared/src/api/wecom.ts
+++ packages/shared/src/api/wecom.ts
@@ -1,1 +1,42 @@
-export class WecomApi { }
+import type { TokenModel } from './dingtalk'
+import { WECOM_BASE_URL } from '../constants'
+import { fetchClient } from '../http'
+
+export interface TokenParams {
+    corpid: string
+    corpsecret: string
+}
+
+export enum MsgTypeEnum {
+    text = 'text',
+    image = 'image',
+    textcard = 'textcard'
+}
+export type MsgType = keyof typeof MsgTypeEnum
+
+export type SendMessageBody = {
+    [key in MsgType]: any
+} & {
+    touser: string
+    toparty?: string
+    totag?: string
+    msgtype: MsgType
+    agentid: number; safe?: number
+    enable_id_trans?: number
+    enable_duplicate_check?: number
+}
+
+export class WecomkApi {
+    static getToken(params: TokenParams) {
+        return fetchClient.get<TokenModel>(`${WECOM_BASE_URL}/cgi-bin/gettoken`, { params })
+    }
+
+    static sendMessage(body: SendMessageBody, token: TokenModel['access_token']) {
+        return fetchClient.post(`${WECOM_BASE_URL}/cgi-bin/message/send`, {
+            body,
+            params: {
+                assess_token: token
+            }
+        })
+    }
+}
--- packages/shared/src/server/wecom.ts
+++ packages/shared/src/server/wecom.ts
@@ -1,1 +1,18 @@
-export class WecomServer { }
+import type { TokenModel } from '../api/dingtalk'
+import type { SendMessageBody, TokenParams } from '../api/wecom'
+import { WecomkApi } from '../api/wecom'
+
+export class WecomServer {
+    private static token: TokenModel['access_token']
+
+    static getToken(params: TokenParams) {
+        return WecomkApi.getToken(params).then(res => {
+            this.token = res.access_token
+            return res
+        })
+    }
+
+    static sendMessage(body:SendMessageBody) {
+        return WecomkApi.sendMessage(body, this.token)
+    }
+}
