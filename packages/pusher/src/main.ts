6a3807a14255f826e9927b30f8a2254e72b5c204
yzydeveloper <wqrdw@qq.com>
2022-08-24 13:19:30 +0800

    feat: set time zone

--- packages/pusher/src/main.ts
+++ packages/pusher/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+    process.env.TZ = 'Asia/Shanghai'
     const app = await NestFactory.create(AppModule)
     await app.listen(3000)
     console.log(`Application is running on: ${await app.getUrl()}`)
