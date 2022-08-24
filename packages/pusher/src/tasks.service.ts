c2b6ed0b868cf956595d209db1de45065cf4a52d
yzydeveloper <wqrdw@qq.com>
2022-08-24 14:43:32 +0800

    test: add Test

--- packages/pusher/src/tasks.service.ts
+++ packages/pusher/src/tasks.service.ts
@@ -1,9 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common'
-import { Cron } from '@nestjs/schedule'
+import { Cron, Interval } from '@nestjs/schedule'
 
 @Injectable()
 export class TasksService {
     private readonly logger = new Logger(TasksService.name)
+
+    @Interval(1000)
+    handleTest() {
+        this.logger.debug('Test>>>Test')
+    }
 
     @Cron('0 30 7 * * *')
     handleMorning() {
