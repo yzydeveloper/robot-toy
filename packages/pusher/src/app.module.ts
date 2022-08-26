51e9513468e11c3f27275e54ec8a1b35a9435d8d
yzydeveloper <wqrdw@qq.com>
2022-08-26 11:01:26 +0800

    feat: support env config

--- packages/pusher/src/app.module.ts
+++ packages/pusher/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ScheduleModule } from '@nestjs/schedule'
+import { ConfigModule } from '@nestjs/config'
 import { TasksModule } from './tasks.module'
 
 @Module({
-    imports: [ScheduleModule.forRoot(), TasksModule],
+    imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), TasksModule],
 })
 export class AppModule {}
