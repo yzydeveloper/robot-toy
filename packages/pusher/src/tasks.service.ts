72ad1c595e50ac64cd598e8a8bbbd73d4b0555a1
yzydeveloper <wqrdw@qq.com>
2022-08-28 09:34:54 +0800

    feat: add debug

--- packages/pusher/src/tasks.service.ts
+++ packages/pusher/src/tasks.service.ts
@@ -10,11 +10,21 @@ export class TasksService {
     @Cron('0 30 7 * * *')
     handleMorning() {
         const { WECOM_APP_ID, DINGTALK_ROBOT_APP_ID, DINGTALK_USERIDS, CITY, TIME_OF_KNOWING } = process.env
+        this.logger.debug(`[WECOM_APP_ID]：${WECOM_APP_ID}`)
+        this.logger.debug(`[DINGTALK_ROBOT_APP_ID]：${DINGTALK_ROBOT_APP_ID}`)
+        this.logger.debug(`[DINGTALK_USERIDS]：${DINGTALK_USERIDS}`)
+        this.logger.debug(`[CITY]：${CITY}`)
+        this.logger.debug(`[TIME_OF_KNOWING]：${TIME_OF_KNOWING}`)
 
         getTextCardMsg({
             city: CITY,
             timeOfKnowing: TIME_OF_KNOWING
         }).then(async params => {
+            this.logger.debug(`[SendMessageParams]：${JSON.stringify({
+                touser: '@all',
+                agentid: parseInt(WECOM_APP_ID, 10),
+                ...params
+            })}`)
             WecomApi.sendMessage({
                 touser: '@all',
                 agentid: parseInt(WECOM_APP_ID, 10),
@@ -25,6 +35,11 @@ export class TasksService {
             city: CITY,
             timeOfKnowing: TIME_OF_KNOWING
         }).then(async params => {
+            this.logger.debug(`[BatchSendOtomsgParams]：${JSON.stringify({
+                robotCode: DINGTALK_ROBOT_APP_ID,
+                userIds: DINGTALK_USERIDS as unknown as string[],
+                ...params
+            })}`)
             DingtalkApi.batchSendOtomsg({
                 robotCode: DINGTALK_ROBOT_APP_ID,
                 userIds: DINGTALK_USERIDS as unknown as string[],
